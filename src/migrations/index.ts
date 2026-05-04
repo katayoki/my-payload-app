f1ae504da6a74058a27d69296fa4b5022f422946
katayoki <jjrcalumpang16@gmail.com>
2026-05-04 18:49:56 +0800

    Initial migrate

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260504_104725 from './20260504_104725';
+
+export const migrations = [
+  {
+    up: migration_20260504_104725.up,
+    down: migration_20260504_104725.down,
+    name: '20260504_104725'
+  },
+];
